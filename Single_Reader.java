912670143341725e3298d27316193cc7599e2a74
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-01-18 15:16:32 +0530

    new problem

---
+++ Single_Reader.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Single_Reader{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		switch(n){
+			case 0:System.out.println("zero");
+			case 1:System.out.println("One");
+			case 2:System.out.println("two");
+			case 3:System.out.println("three");
+			case 4:System.out.println("Four");
+			case 5:System.out.println("five");
+			}
+		}
+}
+					
